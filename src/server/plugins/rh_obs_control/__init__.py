fa830a2a7bf3ff815ba1b859904e0f7d65a3c27a
Paulo Jesus <paulschein@gmail.com>
2023-02-14 00:11:49 +0000

    Add emite_priority_message

--- src/server/plugins/rh_obs_control/__init__.py
+++ src/server/plugins/rh_obs_control/__init__.py
@@ -6,20 +6,28 @@
 
 import logging
 logger = logging.getLogger(__name__)
-import traceback
 import Config
 from eventmanager import Evt
 from plugins.rh_obs_control.obs_manager import NoOBSManager, OBSManager
 import importlib
-#from server import  emit_priority_message                  #FIXME TODO
 
 obs = {}
+SOCKET_IO = None
+
+
+def emite_priority_message(message):
+    ''' Emits message to clients '''
+    emit_payload = {
+        'message': message,
+        'interrupt': False
+    }
+    SOCKET_IO.emit('priority_message', emit_payload)
 
 
 def do_ObsInitialize_fn(args):
-    """Initialize OBS connection"""
-    global obs
+    ''' Initialize OBS connection '''
     logger.info("def do_ObsInitialize_fn(()" )
+    global obs
     obs = NoOBSManager()
     if 'OBS' in Config.GENERAL and 'HOST' in Config.GENERAL['OBS'] and 'PORT' in Config.GENERAL['OBS'] and 'PASSWORD' in Config.GENERAL['OBS']:
         try:
@@ -29,28 +37,23 @@ def do_ObsInitialize_fn(args):
             logger.error("OBS: Error importing obsws_python, please pip install this library manually")
             obs = NoOBSManager()
 
+
 def do_race_start(args):
-    global obs
     logger.info("def do_race_start()")
     if not obs.start():
-        pass
-        #emit_priority_message("OBS: Start Recording Failed", True)     #TODO
+        emite_priority_message("OBS: Start Recording Failed")
+
 
 def do_race_stop(args):
-    global obs
     logger.info("def do_race_stop()")
     if not obs.stop():
-        pass
-        #emit_priority_message("OBS: Stop Recording Failed", True)       #TODO
+        emite_priority_message("OBS: Stop Recording Failed")
 
-def do_race_event(args):
-    print ('Event ', args['event'])
 
 def initialize(**kwargs):
+    global SOCKET_IO
+    SOCKET_IO = kwargs['SOCKET_IO']
     if 'Events' in kwargs:
         kwargs['Events'].on(Evt.STARTUP, 'ObsInitialize', do_ObsInitialize_fn, {}, 90 )
         kwargs['Events'].on(Evt.RACE_START, 'ObsRaceStart', do_race_start, {}, 101 ) # Bloquer / Non bloquer ?
         kwargs['Events'].on(Evt.RACE_STOP, 'ObsRaceStop', do_race_stop, {}, 102 )   # Bloquer / Non bloquer ?
-
-        kwargs['Events'].on(Evt.RACE_STAGE, 'ObsRace_schedule_stage', do_race_event, {'event': 'RACE_STAGE'}, 101 )
-        kwargs['Events'].on(Evt.RACE_FINISH, 'ObsRaceFinish', do_race_event, {'event': 'RACE_FINISH'}, 101 )
